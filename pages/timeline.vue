746fcbdc61e0cb9d6155533a2f743671884621c9
Leonard Cremer <leonard@trooptravel.com>
2022-04-27 21:03:36 +0200

    More changes

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -1,25 +1,32 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="text-gray-800 font-semibold troop">Timeline</div>
-  <div ref="timelineRef">Some text</div>
+  <div>
+    <div class="text-gray-800 font-semibold troop">Timeline</div>
+    <div ref="timelineRef">Some text</div>
+    <div>{{ refItems }}</div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Timeline } from "vis-timeline/standalone";
 import { DataSet } from "vis-data/standalone";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed, watch, watchEffect } from "vue";
 
 const timelineRef = ref(null);
-
-// Create a DataSet (allows two way data-binding)
-var items = new DataSet([
+const itemRef = ref([
   {
     id: 1,
     content: "",
     start: "2014-04-10",
     end: "2014-04-19",
     className: "troop",
-    class: "bg-red-500",
+    editable: {
+      add: true, // add new items by double tapping
+      updateTime: true, // drag items horizontally
+      updateGroup: true, // drag items from one group to another
+      remove: true, // delete an item by tapping the delete button top right
+      overrideItems: false, // allow these options to override item.editable
+    },
   },
   { id: 2, content: "item 2", start: "2014-04-14" },
   { id: 3, content: "item 3", start: "2014-04-18" },
@@ -27,12 +34,53 @@ var items = new DataSet([
   { id: 5, content: "item 5", start: "2014-04-25" },
   { id: 6, content: "item 6", start: "2014-04-27", type: "point" },
 ]);
+// Create a DataSet (allows two way data-binding)
+var items2 = new DataSet(itemRef.value);
+var items: any = ref(
+  new DataSet([
+    {
+      id: 1,
+      content: "",
+      start: "2014-04-10",
+      end: "2014-04-19",
+      className: "troop",
+      editable: {
+        add: true, // add new items by double tapping
+        updateTime: true, // drag items horizontally
+        updateGroup: true, // drag items from one group to another
+        remove: true, // delete an item by tapping the delete button top right
+        overrideItems: false, // allow these options to override item.editable
+      },
+    },
+    { id: 2, content: "item 2", start: "2014-04-14" },
+    { id: 3, content: "item 3", start: "2014-04-18" },
+    { id: 4, content: "item 4", start: "2014-04-16", end: "2014-04-19" },
+  ])
+);
 
 // Configuration for the Timeline
 var options = {};
 
+const refItems = computed(() => {
+  return items.value.get();
+});
+
+watchEffect(() => console.log(items.value.get()));
+
+watch(refItems, (value) => {
+  console.log(refItems.value);
+  items.value.add([
+    { id: 5, content: "item 5", start: "2014-04-25" },
+    { id: 6, content: "item 6", start: "2014-04-27", type: "point" },
+  ]);
+});
+
 onMounted(() => {
   // Create a Timeline
-  var timeline = new Timeline(timelineRef.value, items, options);
+  var timeline = new Timeline(timelineRef.value, items.value, options);
+  timeline.on("select", function (properties) {
+    console.log("selected items: " + properties.items);
+    console.log(items.value.get(1));
+  });
 });
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `plugins/**/*.{js,ts}`,
+    `app.vue`,
   ],
   theme: {
     extend: {},
